5d6a720b7679fe597813faa962ca719005a6fa4a
Peter Toth <106149349+Twixy01@users.noreply.github.com>
2024-06-11 08:33:52 +0200

    filter update main.js
    All password filter now always work together.

--- main.js
+++ main.js
@@ -7,77 +7,83 @@ document.getElementById("len").addEventListener("change", function () {
 });
 
 function contains(id) {
-    if (id.checked) {
-        return true;
-    }
-    return false;
+    return id.checked;
 }
 
 document.getElementById("gen").addEventListener("click", function () {
     let password = "";
     let len = parseInt(document.getElementById("len").value);
 
-    let containsCap = contains(nagybetu);
-    let containsNum = contains(szam);
-    let containsSpec = contains(spec);
+    let containsCap = contains(document.getElementById("nagybetu"));
+    let containsNum = contains(document.getElementById("szam"));
+    let containsSpec = contains(document.getElementById("spec"));
 
-
-    let tomb = [
+    let array = [
         'a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h',
         'i', 'í', 'j', 'k', 'l', 'm', 'n', 'o', 'ó', 'ö',
         'ő', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'ű',
         'v', 'w', 'x', 'y', 'z'
     ];
 
-    let nagybetuk = [
+    let uppercase = [
         'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H',
         'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö',
         'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű',
         'V', 'W', 'X', 'Y', 'Z'
     ];
-    let szamok = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    let speckok = ['!', '@', '#', '$', '%', '&', '*'];
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let specials = ['!', '@', '#', '$', '%', '&', '*'];
 
+    password = generate(len, array, array);
 
     if (containsCap) {
-        tomb.push.apply(tomb, nagybetuk);
+        if (!containsNum && !containsSpec) {
+            merge(array, uppercase); // only uppercase
+        } else if (containsNum && !containsSpec) {
+            merge(array, uppercase, numbers) //uppercase and numbers....
+        } else if (containsSpec && !containsNum) {
+            merge(array, uppercase, specials)
+        } else if (containsNum && containsSpec) {
+            merge(array, uppercase, numbers, specials)
+        }
+    } else if (containsNum) {
+        if (!containsCap && !containsSpec) {
+            merge(array, numbers);
+        } else if (containsSpec && !containsCap) {
+            merge(array, numbers, specials)
+        }
+    } else if (containsSpec) {
+        merge(array, specials);
     }
-    if (containsNum) {
-        tomb.push.apply(tomb, szamok);
-    }
-    if (containsSpec) {
-        tomb.push.apply(tomb, speckok);
-    }
-    shuffle(tomb);
 
-    password = generate(len, tomb);
-    // while ("")
-    // switch (password) {
-    //     case containsCap && !tomb.includes(nagybetuk):
-    //         generate(len, tomb);
-    //         break;
-    // }
-
+    function merge(mainArray, ...arrays) {
+        arrays.forEach(array => {
+            mainArray.push(...array);
+        });
+        password = generate(len, mainArray, mainArray, ...arrays);
+    }
+    
     document.getElementById("passDisplay").value = password;
 });
 
-function shuffle(array) {
-    let currentIndex = array.length - 1;
+function generate(len, array, ...feltetelek) {
+    let password;
+    let helyes;
 
-    while (currentIndex != 0) {
-        let randomIndex = Math.floor(Math.random() * currentIndex);
+    while (true) {
+        password = "";
+        for (let i = 0; i < len; i++) {
+            let random = Math.floor(Math.random() * array.length);
+            password += array[random];
+        }
 
-        [array[currentIndex], [array[randomIndex]]] = [array[randomIndex], [array[currentIndex]]]
-        currentIndex--;
+        helyes = feltetelek.every(feltetel => feltetel.some(element => password.includes(element)));
+
+        if (helyes) {
+            break;
+        }
     }
-}
 
-function generate(len, tomb) {
-    let password = "";
-    for (var i = 0; i < len; i++) {
-        var random = Math.floor(Math.random() * tomb.length);
-        password += tomb[random];
-    }
     return password;
 }
 
@@ -85,5 +91,14 @@ document.getElementById("copy").addEventListener("click", function () {
     let copytext = document.getElementById("passDisplay");
     copytext.select();
     navigator.clipboard.writeText(copytext.value);
-
 });
+
+/*function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+}*/
